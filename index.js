891b4874dbe32cf4bc51ad3ff8fe54c17fbbcbf3
GYANESH KUMAR <Gyanesh5009@gmail.com>
2024-09-15 01:29:35 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -17,13 +16,12 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-//If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🎉 Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🎉";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").innerHTML = "😊 Draw!";
 }
